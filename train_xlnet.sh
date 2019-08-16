8fb41f0501d0f40540f4235fbb2f5b48ae106679
Max Savery <saverymax@gmail.com>
2019-08-16 11:00:18 -0400

    update XLNet training and running

--- train_xlnet.sh
+++ train_xlnet.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-#SBATCH --output=/home/saveryme/chemical_recognition/xlnet_extension_tf/xlnet_logs/slurm_%j.out
-#SBATCH --error=/home/saveryme/chemical_recognition/xlnet_extension_tf/xlnet_logs/slurm_%j.error
-#SBATCH --job-name=XLNET_CER
-#SBATCH --partition=gpu 
-#SBATCH --gres=gpu:p100:1 
-#SBATCH --mem=30g 
-#SBATCH --cpus-per-task=8
-#SBATCH --time=72:00:00
 
-data_dir=/data/saveryme
+data_dir=./data/training_data
+
+mkdir ./model_output/xlnet_chemdner
+mkdir ./model_output/xlnet_gm
+mkdir ./model_output/xlnet_chemdner/results
+mkdir ./model_output/xlnet_gm/results
 
 # train xl net base on chemdner
-python -u /home/saveryme/chemical_recognition/xlnet_extension_tf/run_ner.py \
-    --spiece_model_file=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/spiece.model \
-    --model_config_path=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_config.json \
-    --init_checkpoint=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_model.ckpt \
+python XLNet_annotator.py \
+    --spiece_model_file=./checkpoints/xlnet_cased_L-12_H-768_A-12/spiece.model \
+    --model_config_path=./checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_config.json \
+    --init_checkpoint=./checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_model.ckpt \
     --task_name=CER \
-    --data_dir=${data_dir}/NERdata \
-    --output_dir=${data_dir}/model_output/xl_net_base_chemdner_output \
-    --model_dir=${data_dir}/model_output/xl_net_base_chemdner_output \
+    --output_dir=./model_output/xlnet_chemdner \
+    --model_dir=./model_output/xlnet_chemdner \
+    --data_dir=${data_dir} \
     --input_file=chemdner_CDI_training.txt \
     --max_seq_length=512 \
     --train_batch_size=4 \
@@ -36,14 +33,14 @@ python -u /home/saveryme/chemical_recognition/xlnet_extension_tf/run_ner.py \
     --lower_case=False \
 
 # Train xl net base on gene mentions
-python -u /home/saveryme/chemical_recognition/xlnet_extension_tf/run_ner.py \
-    --spiece_model_file=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/spiece.model \
-    --model_config_path=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_config.json \
-    --init_checkpoint=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_model.ckpt \
+python -XLNet_annotator.py \
+    --spiece_model_file=./checkpoints/xlnet_cased_L-12_H-768_A-12/spiece.model \
+    --model_config_path=./checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_config.json \
+    --init_checkpoint=./checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_model.ckpt \
     --task_name=CER \
-    --data_dir=${data_dir}/NERdata \
-    --output_dir=${data_dir}/model_output/xl_net_base_gm_output \
-    --model_dir=${data_dir}/model_output/xl_net_base_gm_output \
+    --output_dir=./model_output/xlnet_gm \
+    --model_dir=./model_output/xlnet_gm \
+    --data_dir=${data_dir} \
     --input_file=BioC_GM_training.txt \
     --max_seq_length=512 \
     --train_batch_size=4 \
@@ -59,5 +56,3 @@ python -u /home/saveryme/chemical_recognition/xlnet_extension_tf/run_ner.py \
     --do_predict=False \
     --do_export=False \
     --lower_case=False \
-
-
