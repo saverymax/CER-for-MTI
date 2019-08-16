e523427b325238972f248729fca6162c931f4ffe
Max Savery <saverymax@gmail.com>
2019-08-16 11:34:13 -0400

    Update XLNet

--- run_xlnet.sh
+++ run_xlnet.sh
@@ -1,42 +1,15 @@
-data_dir=/data/saveryme
+data_dir=./data/ChEMFAM_corpus/*.txt
 
-# Run chemdner xlnet base on our corpus
-#python run_ner.py \
-#    --spiece_model_file=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/spiece.model \
-#    --model_config_path=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_config.json \
-#    --init_checkpoint=${data_dir}/model_output/xl_net_base_chemdner_output/model.ckpt-8000 \
-#    --task_name=CER \
-#    --data_dir=${data_dir}/NERdata/citations/*.txt \
-#    --output_dir=${data_dir}/model_output/xl_net_base_chemdner_output \
-#    --model_dir=${data_dir}/model_output/xl_net_base_chemdner_output \
-#    --output_file=results/xlnet_chemdner_tokenized_test.txt \
-#    --max_seq_length=512 \
-#    --train_batch_size=4 \
-#    --num_hosts=1 \
-#    --num_core_per_host=1 \
-#    --learning_rate=2e-5 \
-#    --train_steps=2500 \
-#    --warmup_steps=100 \
-#    --save_steps=500 \
-#    --do_train=False \
-#    --do_eval=False \
-#    --do_predict=True \
-#    --lower_case=False \
-#    --do_export=False
-
-output_file_dir=/data/saveryme/model_output/xl_net_base_chemdner_output/results
-python convert_output.py --input ${output_file_dir}/xlnet_chemdner_tokenized_test.txt --output ${output_file_dir}/xlnet_chemdner_test.txt 
-
-# Run gene mention xlnet base on our corpus
-python run_ner.py \
-    --spiece_model_file=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/spiece.model \
-    --model_config_path=${data_dir}/model_checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_config.json \
-    --init_checkpoint=${data_dir}/model_output/xl_net_base_gm_output/model.ckpt-8000 \
+# Run chemdner xlnet
+python XLNet_annotator.py \
+    --spiece_model_file=./checkpoints/xlnet_cased_L-12_H-768_A-12/spiece.model \
+    --model_config_path=./checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_config.json \
+    --init_checkpoint=./model_output/xl_net_base_chemdner_output/model.ckpt-8000 \
     --task_name=CER \
-    --data_dir=${data_dir}/NERdata/citations/*.txt \
-    --output_dir=${data_dir}/model_output/xl_net_base_gm_output \
-    --model_dir=${data_dir}/model_output/xl_net_base_gm_output \
-    --output_file=results/xlnet_gm_tokenized_test.txt \
+    --data_dir=${data_dir} \
+    --output_dir=${data_dir}/model_output/xlnet_chemdner \
+    --model_dir=${data_dir}/model_output/xlnet_chemdner \
+    --output_file=results/xlnet_chemdner_tokenized_test.txt \
     --max_seq_length=512 \
     --train_batch_size=4 \
     --num_hosts=1 \
@@ -51,6 +24,32 @@ python run_ner.py \
     --lower_case=False \
     --do_export=False
 
+output_file_dir=/data/saveryme/model_output/xl_net_base_chemdner_output/results
+python convert_output.py --input ${output_file_dir}/xlnet_chemdner_tokenized_test.txt --output ${output_file_dir}/xlnet_chemdner_test.txt 
+
+# Run gene mention xlnet base on our corpus
+python XLNet_annotator.py \
+    --spiece_model_file=./checkpoints/xlnet_cased_L-12_H-768_A-12/spiece.model \
+    --model_config_path=./checkpoints/xlnet_cased_L-12_H-768_A-12/xlnet_config.json \
+    --init_checkpoint=./model_output/xl_net_base_gm_output/model.ckpt-8000 \
+    --task_name=CER \
+    --data_dir=${data_dir} \
+    --output_dir=${data_dir}/model_output/xlnet_gm \
+    --model_dir=${data_dir}/model_output/xlnet_gm \
+    --output_file=results/xlnet_gm_tokenized_test.txt \
+    --max_seq_length=512 \
+    --train_batch_size=4 \
+    --num_hosts=1 \
+    --num_core_per_host=1 \
+    --learning_rate=2e-5 \
+    --train_steps=2500 \
+    --warmup_steps=100 \
+    --save_steps=500 \
+    --do_train=False \
+    --do_eval=False \
+    --do_predict=True \
+    --lower_case=False \
+    --do_export=False
 
 output_file_dir=/data/saveryme/model_output/xl_net_base_gm_output/results
 python convert_output.py --input ${output_file_dir}/xlnet_gm_tokenized_test.txt --output ${output_file_dir}/xlnet_gm_test.txt 
